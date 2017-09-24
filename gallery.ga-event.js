f77b32356c1e861fffe14e6e8e385641862cad79
Trevor Bice <trevorbicewebdesign@gmail.com>
2017-09-24 14:48:37 -0700

    Lots o random commits

--- gallery.ga-event.js
+++ gallery.ga-event.js
@@ -2,16 +2,10 @@
 
 // Events Generated Are summarized below
 // console.log('Show More button activated');
-// console.log("Show More Pagination Inserted");
-
 // console.log('Menu Activated');
-// console.log('Menu Deactivated');
-
-// console.log("Uploader Page Visited");
+// console.log('Menu Deactivated')
+// console.log("Uploader Page Visited");	
 // console.log("Submit Button Clicked");
-// console.log("Upload Form Submitted");	
-
-// console.log("Credits Viewed");
 
 jQuery( document ).ready(function() {
 	// Event if the 'Show More' Button has been clicked
@@ -59,6 +53,7 @@ jQuery( document ).ready(function() {
 			
 			jQuery('#upload-form .form-group.required, #upload-form .form-group.has-error').each(function(index,element){
 					// This must be the file upload field
+
 					if( jQuery(this).hasClass('field-uploaderform-imagefile') && ( jQuery(this).hasClass('has-error') || jQuery(this).find('input').val() == '') ){
 						error = jQuery(this).find('.help-block-error').text();
 						
@@ -74,41 +69,124 @@ jQuery( document ).ready(function() {
 					else if( jQuery(this).hasClass('field-uploaderform-fname') && ( jQuery(this).hasClass('has-error') || jQuery(this).find('input').val() == '') ){
 						error = "Please enter a first name";
 						console.log(error);
+						if(jQuery(this).find('input').val() == ''){
+							jQuery(this).addClass('has-error');
+							jQuery(this).find('.help-block-error').html("Required field.");
+						
+						}
+						gtag('event', 'uploader_form_error_file', {
+							'event_category': 'Uploader Form',
+							'event_label': 'Uploader Form - File - '+error
+						});
+						
 					}
 				 // Last Name
 					else if( jQuery(this).hasClass('field-uploaderform-lname') && ( jQuery(this).hasClass('has-error') || jQuery(this).find('input').val() == '') ){
 						error = "Please enter a last name";
 						console.log(error);
+						if(jQuery(this).find('input').val() == ''){
+							jQuery(this).addClass('has-error');
+							jQuery(this).find('.help-block-error').html("Required field.");
+						
+						}
+						gtag('event', 'uploader_form_error_file', {
+							'event_category': 'Uploader Form',
+							'event_label': 'Uploader Form - File - '+error
+						});
 					}
 					// Email
 					else if( jQuery(this).hasClass('field-uploaderform-photographersemail') && ( jQuery(this).hasClass('has-error') || jQuery(this).find('input').val() == '') ){
 						error = "Please enter an email";
 						console.log(error);
+						if(jQuery(this).find('input').val() == ''){
+							jQuery(this).addClass('has-error');
+							jQuery(this).find('.help-block-error').html("Required field.");
+						
+						}
+						gtag('event', 'uploader_form_error_file', {
+							'event_category': 'Uploader Form',
+							'event_label': 'Uploader Form - File - '+error
+						});
 					}
 					// Photo Title
-					else if( jQuery(this).hasClass('field-uploaderform-photographersemail') && ( jQuery(this).hasClass('has-error') || jQuery(this).find('input').val() == '') ){
+					else if( jQuery(this).hasClass('field-uploaderform-title') && ( jQuery(this).hasClass('has-error') || jQuery(this).find('input').val() == '') ){
 						error = "Please enter a photo title";
 						console.log(error);
+						if(jQuery(this).find('input').val() == ''){
+							jQuery(this).addClass('has-error');
+							jQuery(this).find('.help-block-error').html("Required field.");
+						
+						}
+						gtag('event', 'uploader_form_error_file', {
+							'event_category': 'Uploader Form',
+							'event_label': 'Uploader Form - File - '+error
+						});
 					}
 					// Caption
 					else if( jQuery(this).hasClass('field-uploaderform-captionfrommeta') && ( jQuery(this).hasClass('has-error') || jQuery(this).find('input').val() == '') ){
 						error = "Please enter a caption";  
 						console.log(error);
+						if(jQuery(this).find('input').val() == ''){
+							jQuery(this).addClass('has-error');
+							jQuery(this).find('.help-block-error').html("Required field.");
+						
+						}
+						gtag('event', 'uploader_form_error_file', {
+							'event_category': 'Uploader Form',
+							'event_label': 'Uploader Form - File - '+error
+						});
 					}
 					// Year
 					else if( jQuery(this).hasClass('field-uploaderform-year') && ( jQuery(this).hasClass('has-error') || jQuery(this).find('select').val() == '') ){
 						error = "Please enter a year";
 						console.log(error);
+						if(jQuery(this).find('select').val() == ''){
+							jQuery(this).addClass('has-error');
+							jQuery(this).find('.help-block-error').html("Required field.");
+						}
+						gtag('event', 'uploader_form_error_file', {
+							'event_category': 'Uploader Form',
+							'event_label': 'Uploader Form - File - '+error
+						});
 					}
 					// Terms of Service
 					else if( jQuery(this).hasClass('field-uploaderform-acceptcheckbox') && ( jQuery(this).hasClass('has-error') || !jQuery(this).find('input').attr("checked") ) ){
 						error = "Please check the terms of service";
 						console.log(error);
+						if(!jQuery(this).find('input').attr("checked")){
+							jQuery(this).addClass('has-error');
+							jQuery(this).find('.help-block-error').html("Required field.");
+						}
+						gtag('event', 'uploader_form_error_file', {
+							'event_category': 'Uploader Form',
+							'event_label': 'Uploader Form - File - '+error
+						});
 					}
 				 
 			});
 				
 		});
+		
+		
+		jQuery(".upload_message").change(function() {
+			if(jQuery(this).html()=='File sample-image.jpg has been successfully uploaded.') {
+				
+				console.log("File Uploaded Successfully");
+				gtag('event', 'uploader_form_error_file', {
+					'event_category': 'Uploader Form',
+					'event_label': 'Uploader Form - File - '+error
+				});
+			}
+		});
+		if(jQuery(".upload_message").html()=='File sample-image.jpg has been successfully uploaded.') {
+			console.log("File Uploaded Successfully");
+			gtag('event', 'uploader_form_error_file', {
+				'event_category': 'Uploader Form',
+				'event_label': 'Uploader Form - File - '+"File Uploaded Successfully"
+			});
+		}
+		// 
+		
 		/*
 		jQuery('mydiv').bind("DOMSubtreeModified",function(){
 			alert('changed');
